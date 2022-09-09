b66d5db8dc3378e15e8e9b88870055c01552a6c4
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-09 19:27:22 +0300

    Create book.dart

---
+++ lib/src/models/book.dart
@@ -0,0 +1,82 @@
+class Book {
+  final String title;
+  final String urlImage;
+
+  const Book({required this.title, required this.urlImage});
+}
+
+const allBooks = [
+  Book(
+    title: "İyi Hissetmek",
+    urlImage:
+        "https://img.kitapyurdu.com/v1/getImage/fn:11261099/wh:true/wi:256",
+  ),
+  Book(
+      title: "Posta Kutusundaki Mızıka",
+      urlImage:
+          "https://img.kitapyurdu.com/v1/getImage/fn:11545735/wh:true/wi:220"),
+  Book(
+      title: "Atomik Alışkanlıklar",
+      urlImage:
+          "https://img.kitapyurdu.com/v1/getImage/fn:11221036/wh:true/wi:220"),
+  Book(
+      title: "Son Nefes Havaya Karışmadan",
+      urlImage:
+          "https://img.kitapyurdu.com/v1/getImage/fn:2655734/wh:true/wi:220"),
+  Book(
+      title: "Düşünceler",
+      urlImage: "https://1k-cdn.com/k/resimler/kitaplar/34817_1430490872.jpg"),
+  Book(
+      title: "Zamanın Daha Kısa Tarihi",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/7947_Zamanin_Daha_Kisa_Tarihi-Leonard_Mlodinow_Stephen_W_Hawking274.jpg"),
+  Book(
+      title: "Aklın Türleri",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/Aklin-Turleri_26279_1418497105.jpg"),
+  Book(
+      title: "İnsan Olmanın Psikolojisi",
+      urlImage: "https://1k-cdn.com/resimler/kitaplar/15009_1426706231.jpg"),
+  Book(
+      title: "Kendinin Ağacı",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/209154_b7680_1599830570.jpg"),
+  Book(
+      title: "Lavinia",
+      urlImage: "https://1k-cdn.com/resimler/kitaplar/30074_1425238716.jpg"),
+  Book(
+      title: "Ah'lar Ağacı",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/Ahlar-Agaci_19456_1400435169.jpg"),
+  Book(
+      title: "Bir Kapı Önünde",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/568685_b3c37_1567698294.jpg"),
+  Book(
+      title: "Başkalarının Acısına Bakmak",
+      urlImage:
+          "https://1k-cdn.com/k/resimler/kitaplar/7048_Baskalarinin_Acisina_Bakmak-Susan_Sontag879.jpg"),
+  Book(
+      title: "Özgürlük",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/36229_PeYfq_1492450904.jpg"),
+  Book(
+      title: "Zeka",
+      urlImage: "https://1k-cdn.com/resimler/kitaplar/30889_1427131266.jpg"),
+  Book(
+      title: "Seninle Başlamadı",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/56586_1660453999_x3Gr7.jpg"),
+  Book(
+      title: "Nar Ağacı",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/Nar-Agaci_1584_1392582019.jpg"),
+  Book(
+      title: "Google",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/Google-Hikayesi_19615_1401267558.jpg"),
+  Book(
+      title: "Bir Ömür Nasıl Yaşanır",
+      urlImage:
+          "https://1k-cdn.com/resimler/kitaplar/56586_1653916394_2NqzY.jpg")
+];
